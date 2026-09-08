4910d2bc3deb0861027e46206c8ee77493cd6bdb
Soucelover <mr.ktotov@gmail.com>
2026-09-08 21:46:14 +0300

    Rewrite build.ps1 to follow better practices

--- build.ps1
+++ build.ps1
@@ -1,19 +1,23 @@
 # PowerShell 7.0 is recommended
 
+param (
+    [String]$Name = "HelloWorld"
+)
+
 $ErrorActionPreference = "Stop"
 Set-StrictMode -Version Latest
 $PSNativeCommandUseErrorActionPreference = $true
 
 Remove-Item -Path ".\bin\*" -Recurse -Force -ErrorAction SilentlyContinue
 
+# Compilation
 javac -d bin (Get-ChildItem -Path ".\src" -Filter "*.java" -Recurse -File)
-Copy-Item ".\src\manifest.txt" ".\bin\"
+Copy-Item ".\src\MANIFEST.MF" ".\bin\"
 
-$old_location = (Get-Location)
+# Building JAR
+Push-Location ".\bin"
+jar cvfm .\$Name.jar .\MANIFEST.MF (Get-ChildItem -Filter "*.class" -Recurse -File | Resolve-Path -Relative)
+Pop-Location
 
-Set-Location ".\bin"
-jar cvfm .\HelloWorld.jar .\manifest.txt (Get-ChildItem -Filter "*.class" -Recurse -File | Resolve-Path -Relative)
-
-Set-Location $old_location
-
-Write-Host "Now, you can hello your world with 'java -jar .\bin\HelloWorld.jar'"
+Write-Host "`nLaunching the application..."
+java -jar .\bin\$Name.jar
