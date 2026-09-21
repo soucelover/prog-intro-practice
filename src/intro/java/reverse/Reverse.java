ea6ae90633d2871c04ee4c681a6ddd3384128861
Soucelover <mr.ktotov@gmail.com>
2026-09-21 20:04:17 +0300

    Write the Reverse algorithm overall

--- src/intro/java/reverse/Reverse.java
+++ src/intro/java/reverse/Reverse.java
@@ -1,7 +1,46 @@
 package intro.java.reverse;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Reverse {
   public static void main(String[] args) {
+    ArrayList<int[]> numbers = parseNumbers();
 
+    for (int i = numbers.size() - 1; i >= 0; --i) {
+      for (int j = numbers.get(i).length - 1; j >= 0; --j) {
+        System.out.println(numbers.get(i)[j]);
+      }
+    }
+  }
+
+  private static ArrayList<int[]> parseNumbers() {
+    Scanner scanner = new Scanner(System.in);
+
+    ArrayList<int[]> numbers = new ArrayList<>();
+
+    while (scanner.hasNextLine()) {
+      String line = scanner.nextLine();
+
+      int[] row = parseSingleLine(line);
+      numbers.add(row);
+    }
+
+    return numbers;
+  }
+
+  private static int[] parseSingleLine(String line) {
+    ArrayList<Integer> numbers = new ArrayList<>();
+
+    // TODO: Write number parsing algorithm
+
+    // Convert into primitive array to reduce memory usage
+    int[] result = new int[numbers.size()];
+
+    for (int i = 0; i < result.length; ++i) {
+      result[i] = numbers.get(i);
+    }
+
+    return result;
   }
 }
